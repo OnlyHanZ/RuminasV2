822334881e56488407d9ec5e5d899d9597c37354
Rimuru Tempest <108761694+OnlyHanZ@users.noreply.github.com>
2022-11-28 02:52:56 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -17,8 +17,8 @@ await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard
 if (command == 'creator') {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `arifofc19@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://rifxz19.github.io/`, `👤 Gada pawang nih senggol dong 😔`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://blog.com/Arifzyn/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me `, `arifofc19@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://github.io/`, `OnlyOwnerr`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me `, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://github.com/RyhnXD`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
   ], fkontak)
   await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
   } catch {
